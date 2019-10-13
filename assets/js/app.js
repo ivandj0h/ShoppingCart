08c569bcf38b52196c81e4a9d837a60e3e26034a
ivandi1980 <ivandi.djoh@gmail.com>
2019-10-13 21:37:18 +0800

    replace old screenshoot

--- assets/js/app.js
+++ assets/js/app.js
@@ -94,10 +94,10 @@ function updateCartTotal() {
         var cartRow = cartRows[i]
         var priceElement = cartRow.getElementsByClassName('cart-price')[0]
         var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
-        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        var price = parseFloat(priceElement.innerText.replace('Rp', ''))
         var quantity = quantityElement.value
         total = total + (price * quantity)
     }
     total = Math.round(total * 100) / 100
-    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+    document.getElementsByClassName('cart-total-price')[0].innerText = 'Rp' + total
 }
